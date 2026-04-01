0dc9406a56bd44e2bb726b88d09b70239a01a5ab
CrafterStudioIndia <craftertechstudio@gmail.com>
2026-04-01 16:18:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ if (typeof process !== 'undefined' && !process.browser && process.platform !== '
 }
 
 module.exports = require('./lib/loader.js')
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+  host: 'AuraSMPbyADMINAURA.aternos.me', // Change this to your Aternos IP
+  username: 'AuraSentinel_Bot',      // The name of your 24/7 bot
+  version: '26.1'                  // Matches your Spigot 26.1
+})
+
+// Movement & Jumping to prevent AFK kicks
+bot.on('spawn', () => {
+  console.log('TheAdminAura Bot is online and moving!')
+  
+  setInterval(() => {
+    // Jump every 30 seconds
+    bot.setControlState('jump', true)
+    setTimeout(() => bot.setControlState('jump', false), 500)
+    
+    // Swing arm to look "active"
+    bot.swingArm()
+    
+    // Randomly move forward for a split second
+    bot.setControlState('forward', true)
+    setTimeout(() => bot.setControlState('forward', false), 200)
+  }, 30000) 
+})
+
+// Auto-reconnect if the server restarts
+bot.on('end', () => {
+  console.log('Bot disconnected. Reconnecting in 30 seconds...')
+  setTimeout(() => process.exit(), 30000) 
+})
